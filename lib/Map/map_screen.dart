94e247d143d5532927932531061bb18919abaffc
GoogleDownload <joseph.fanous@uoit.net>
2020-11-07 22:54:25 -0500

    Maps added

--- lib/Map/map_screen.dart
+++ lib/Map/map_screen.dart
@@ -1,8 +1,22 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../settings_btn.dart';
 import '../drawer.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +25,13 @@ class MapView extends StatelessWidget {
         actions: [SettingsBtn()],
       ),
       drawer: DrawerMenu(),
-      body: null,
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
