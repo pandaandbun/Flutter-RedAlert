e9ceb171733ffc5df6983cd0a0ae8b15232f774e
Joseph Fanous <joseph.fanous@uoit.net>
2020-12-03 20:13:32 -0500

    map locations

--- lib/Breakdown/Data Table/data_table_source.dart
+++ lib/Breakdown/Data Table/data_table_source.dart
@@ -67,7 +67,7 @@ class DataSource extends DataTableSource {
       );
     }
   }
-  
+
   @override
   bool get isRowCountApproximate => false;
 
--- lib/Map/map_screen.dart
+++ lib/Map/map_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
+import 'package:Red_Alert/Database/missing_person_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart' as geo;
 import 'package:google_api_availability/google_api_availability.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+//import 'package:location/location.dart';
+import 'package:geocoding/geocoding.dart';
 
 import '../settings_btn.dart';
 import '../drawer.dart';
@@ -14,17 +18,35 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  Location location = new Location();
+  //Location location = new Location();
   bool _serviceEnabled;
-  PermissionStatus _permissionGranted;
-  LocationData _locationData;
+  //PermissionStatus _permissionGranted;
+  //LocationData _locationData;
+  Set<Marker> _markers = {};
+  int mid = 1;
+  BitmapDescriptor pinLocationIcon;
+  Completer<GoogleMapController> _controller = Completer();
+  //final coordinates = new Coordinates(1.10, 45.50);
+  var _geolocator = geo.Geolocator();
+  Person person;
+  MissingPeopleModel missingPeople = MissingPeopleModel();
+  Stream stream;
 
   @override
   void initState() {
     super.initState();
-    _checkLocationPermission();
+    //_checkLocationPermission();
+    //_locationData = _locationData;
+    stream = missingPeople.getCityProvince();
   }
 
+  void setCustomMapPin() async {
+    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/destination_map_marker.png');
+  }
+
+/*
   // Check Location Permissions, and get my location
   void _checkLocationPermission() async {
     _serviceEnabled = await location.serviceEnabled();
@@ -43,11 +65,51 @@ class _MapViewState extends State<MapView> {
     }
     _locationData = await location.getLocation();
   }
+*/
 
-  Completer<GoogleMapController> _controller = Completer();
+  Person _buildPerson(DocumentSnapshot data) {
+    return Person.fromMap(data.data(), reference: data.reference);
+  }
+
+  void _setMarker() {
+    stream.listen(
+      (element) {
+        element.documents.forEach((snapshot) {
+          Person p = _buildPerson(snapshot);
+          String city = p.city;
+          String prov = p.province;
+          String ady = '$city, $prov';
+          print('LOOOOOOOOOOOL $ady');
+          _geolocator
+              .placemarkFromAddress(ady)
+              .then((List<geo.Placemark> places) {
+            print('Forward geocoding results:');
+            print('LOOOOOOOOOOOL $ady');
+            for (geo.Placemark place in places) {
+              if (place.position.latitude != null &&
+                  place.position.longitude != null) {
+                setState(() {
+                  _markers.add(
+                    Marker(
+                      markerId: MarkerId('Marker Id: $mid'),
+                      position: LatLng(
+                          place.position.latitude, place.position.longitude),
+                      icon: pinLocationIcon,
+                    ),
+                  );
+                  mid++;
+                });
+              }
+            }
+          }).catchError((ignored) {});
+        });
+      },
+    );
+  }
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(43.642565, -79.3870567),
-    zoom: 15,
+    zoom: 14.4746,
   );
 
   Future<bool> _isGoogleAvailable() async {
@@ -88,8 +150,10 @@ class _MapViewState extends State<MapView> {
 
   Widget googleMap() => GoogleMap(
       mapType: MapType.hybrid,
+      markers: _markers,
       initialCameraPosition: _kGooglePlex,
       onMapCreated: (GoogleMapController controller) {
         _controller.complete(controller);
+        _setMarker();
       });
 }
