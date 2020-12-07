e32d308a224d71916ff34bc9cbbc8f1087d7f467
trantienanhta <trantienanh.ta@protonmail.com>
2020-12-07 17:01:32 -0500

    FEAT - map, geolocation, geocoding

--- lib/UI/Map/map_screen.dart
+++ lib/UI/Map/map_screen.dart
@@ -1,159 +1,133 @@
-import 'dart:async';
-
-import 'package:Red_Alert/Database/missing_person_database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart' as geo;
-import 'package:google_api_availability/google_api_availability.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:location/location.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:latlong/latlong.dart';
+
+import '../../Database/missing_person_database.dart';
 
 import '../settings_btn.dart';
 import '../drawer.dart';
 
-class MapView extends StatefulWidget {
+import 'map_box.dart';
+
+class MapScreen extends StatefulWidget {
+  final MissingPeopleModel missingPeopleModel = MissingPeopleModel();
+  final MapController mapController = MapController();
+
   @override
-  _MapViewState createState() => _MapViewState();
+  _MapScreenState createState() => _MapScreenState();
 }
 
-class _MapViewState extends State<MapView> {
-  //Location location = new Location();
-  bool _serviceEnabled;
-  //PermissionStatus _permissionGranted;
-  //LocationData _locationData;
-  Set<Marker> _markers = {};
-  int mid = 1;
-  BitmapDescriptor pinLocationIcon;
-  Completer<GoogleMapController> _controller = Completer();
-  //final coordinates = new Coordinates(1.10, 45.50);
-  var _geolocator = geo.Geolocator();
-  Person person;
-  MissingPeopleModel missingPeople = MissingPeopleModel();
-  Stream stream;
+class _MapScreenState extends State<MapScreen> {
+  String city = '';
+  String province = '';
 
-  @override
-  void initState() {
-    super.initState();
-    //_checkLocationPermission();
-    //_locationData = _locationData;
-    stream = missingPeople.getCityProvince();
-  }
-
-  void setCustomMapPin() async {
-    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(devicePixelRatio: 2.5),
-        'assets/destination_map_marker.png');
-  }
-
-/*
-  // Check Location Permissions, and get my location
-  void _checkLocationPermission() async {
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-    _locationData = await location.getLocation();
-  }
-*/
-
-  Person _buildPerson(DocumentSnapshot data) {
-    return Person.fromMap(data.data(), reference: data.reference);
-  }
-
-  void _setMarker() {
-    stream.listen(
-      (element) {
-        element.documents.forEach((snapshot) {
-          Person p = _buildPerson(snapshot);
-          String city = p.city;
-          String prov = p.province;
-          String ady = '$city, $prov';
-          print('LOOOOOOOOOOOL $ady');
-          _geolocator
-              .placemarkFromAddress(ady)
-              .then((List<geo.Placemark> places) {
-            print('Forward geocoding results:');
-            print('LOOOOOOOOOOOL $ady');
-            for (geo.Placemark place in places) {
-              if (place.position.latitude != null &&
-                  place.position.longitude != null) {
-                setState(() {
-                  _markers.add(
-                    Marker(
-                      markerId: MarkerId('Marker Id: $mid'),
-                      position: LatLng(
-                          place.position.latitude, place.position.longitude),
-                      icon: pinLocationIcon,
-                    ),
-                  );
-                  mid++;
-                });
-              }
-            }
-          }).catchError((ignored) {});
-        });
-      },
+  void _moveMapCenter(Position position) {
+    var currentZoom = widget.mapController.zoom;
+    widget.mapController.move(
+      LatLng(position.latitude, position.longitude),
+      currentZoom,
     );
   }
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(43.642565, -79.3870567),
-    zoom: 14.4746,
-  );
-
-  Future<bool> _isGoogleAvailable() async {
-    GooglePlayServicesAvailability google = await GoogleApiAvailability.instance
-        .checkGooglePlayServicesAvailability();
-    String status = google.toString().split('.').last;
-
-    return status != "serviceInvalid";
+  void _reDrawWhenALocationIsSet(String _city, String _province) {
+    if (_city.isNotEmpty) {
+      setState(() {
+        city = _city;
+        province = _province;
+      });
+    }
   }
+
+  String _getProvinceShortCode(String province) {
+    if (province == "Ontario")
+      return "ON";
+    else if (province == "British Columbia")
+      return "BC";
+    else if (province == "Québec")
+      return "QC";
+    else if (province == "Manitoba")
+      return "MB";
+    else if (province == "Saskatchewan")
+      return "SK";
+    else if (province == "Alberta")
+      return "AB";
+    else if (province == "Yukon")
+      return "YK";
+    else if (province == "Northwest Territories")
+      return "NT";
+    else if (province == "Nunavut")
+      return "NU";
+    else if (province == "Newfoundland &amp; Labrador")
+      return "NL";
+    else if (province == "Nova Scotia")
+      return "NS";
+    else if (province == "New Brunswick")
+      return "NB";
+    else if (province == "Prince Edward Island")
+      return "PE";
+    else
+      return "";
+  }
+
+  Future<Map> _getCurrentlocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> address =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    return {
+      'pos': position,
+      'city': address[0].locality,
+      'province': _getProvinceShortCode(address[0].administrativeArea)
+    };
+  }
+
+  void _setCurrentLocation() async {
+    String _city = "";
+    String _province = "";
+    try {
+      Map locs = await _getCurrentlocation();
+      _moveMapCenter(locs['pos']);
+      _city = locs['city'];
+      _province = locs['province'];
+    } catch (e) {
+      print("ERROR");
+    }
+
+    _reDrawWhenALocationIsSet(_city, _province);
+  }
+
+  // --------------------------------------------------------------
 
   @override
   Widget build(BuildContext context) {
-    Future<bool> gooleAvailable = _isGoogleAvailable();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Map'),
         actions: [SettingsBtn()],
       ),
       drawer: DrawerMenu(),
-      body: futureMap(gooleAvailable),
+      body: _futureMap(),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add), onPressed: () => _setCurrentLocation()),
     );
   }
 
-  Widget futureMap(Future<bool> gooleAvailable) => FutureBuilder(
-      future: gooleAvailable,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          if (snapshot.data) {
-            return googleMap();
+  Widget _futureMap() => FutureBuilder(
+        future: widget.missingPeopleModel.getPeopleWhereCityAndProvince(
+          city,
+          province: province,
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MapBox(snapshot.data, widget.mapController);
           } else {
-            return Text("Device has no Google API");
+            return _loadingIcon();
           }
-        } else {
-          return Text("Checking for Google API");
-        }
-      });
+        },
+      );
 
-  Widget googleMap() => GoogleMap(
-      mapType: MapType.hybrid,
-      markers: _markers,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-        _setMarker();
-      });
+  Widget _loadingIcon() => Center(child: CircularProgressIndicator());
 }
