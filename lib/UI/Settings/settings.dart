b22483471db4cd994cf1e1955eacc0ca8cf3bd5e
jessquerido <jessica.querido@ontariotechu.net>
2020-12-09 00:36:16 -0500

    english/french options in settings

--- lib/UI/Settings/settings.dart
+++ lib/UI/Settings/settings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:Red_Alert/UI/drawer.dart';
 import '../notification.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -20,122 +21,160 @@ class SettingsPage extends State<Settings> {
     notification.init();
 
     return Scaffold(
-        appBar: AppBar(title: Text(FlutterI18n.translate(context, "drawer.settings"))),
+        appBar: AppBar(title: Text(FlutterI18n.translate(context, "settings.title"))),
         drawer: DrawerMenu(),
         body: SingleChildScrollView(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(height: 10),
-            Card(
-              elevation: 8,
-              margin: const EdgeInsets.fromLTRB(30, 5, 30, 5),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(
-                      Icons.lock_outline,
-                      color: Colors.red,
-                    ),
-                    title: Text('Change Password'),
-                    trailing: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.red,
-                    ),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/password');
-                    },
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 10),
+                Card(
+                  elevation: 8,
+                  margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(
+                          Icons.lock_outline,
+                          color: Colors.red,
+                        ),
+                        title: Text('Change Password'),
+                        trailing: Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.red,
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, '/password');
+                        },
+                      ),
+                      _solidLine(),
+                      ListTile(
+                        leading: Icon(
+                          Icons.brightness_medium,
+                          color: Colors.red,
+                        ),
+                        title: Text('Change Theme'),
+                        trailing: Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.red,
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, '/theme');
+                        },
+                      ),
+                    ],
                   ),
-                  _solidLine(),
-                  ListTile(
-                    leading: Icon(
-                      Icons.brightness_medium,
-                      color: Colors.red,
-                    ),
-                    title: Text('Change Theme'),
-                    trailing: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.red,
-                    ),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/theme');
-                    },
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Language Settings",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Notification Settings",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-              ),
-            ),
-            SwitchListTile(
-              activeColor: Colors.red,
-              contentPadding: const EdgeInsets.all(0),
-              value: _isOn1,
-              onChanged: (bool value) {
-                if (value) {
-                  notification.sendNotificationNow(
-                    "Test Notification",
-                    "This is an instant notification",
-                    // "Received"
-                  );
-                }
-                setState(() {
-                  _isOn1 = value;
-                });
-              },
-              title: Text('Instant Notifications'),
-              secondary:
-                  const Icon(Icons.notification_important, color: Colors.red),
-            ),
-            SwitchListTile(
-              activeColor: Colors.red,
-              contentPadding: const EdgeInsets.all(0),
-              value: _isOn2,
-              onChanged: (bool value) {
-                setState(() {
-                  _isOn2 = value;
-                });
-              },
-              title: Text('Featured Persons'),
-              secondary:
-                  const Icon(Icons.notification_important, color: Colors.red),
-            ),
-            SwitchListTile(
-              activeColor: Colors.red,
-              contentPadding: const EdgeInsets.all(0),
-              value: _isOn3,
-              onChanged: (bool value) {
-                setState(() {
-                  _isOn3 = value;
-                });
-              },
-              title: Text('Receive Notifications'),
-              secondary:
-                  const Icon(Icons.notification_important, color: Colors.red),
-            ),
-            SwitchListTile(
-              activeColor: Colors.red,
-              contentPadding: const EdgeInsets.all(0),
-              value: _isOn4,
-              onChanged: (bool value) {
-                setState(() {
-                  _isOn4 = value;
-                });
-              },
-              title: Text('Receive Notifications'),
-              secondary:
-                  const Icon(Icons.notification_important, color: Colors.red),
-            ),
-          ],
-        )));
+                ),
+                Column(
+                  children: <Widget> [
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlineButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget> [
+                            Text(
+                              'English',
+                              textScaleFactor: 1.2,
+                            ),
+                            (true) ? Icon(Icons.check) : Text(""), //update this to actually check language
+                          ]
+                        ),
+                        shape: RoundedRectangleBorder(  
+                          borderRadius: BorderRadius.circular(15)),
+                        onPressed: () async {
+                          print('Language Select: English');
+                          Locale newLocale = Locale('en');
+                          await FlutterI18n.refresh(context, newLocale);
+                          setState(() {});
+                        },
+                      )
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlineButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget> [
+                            Text(
+                            'French',
+                            textScaleFactor: 1.2,
+                            ),
+                            (false) ? Icon(Icons.check) : Text(""), //update this to actually check language
+                          ]
+                        ),
+                        shape: RoundedRectangleBorder(  
+                          borderRadius: BorderRadius.circular(15)),
+                        onPressed: () async {
+                          print('Language Select: French');
+                          Locale newLocale = Locale('fr');
+                          await FlutterI18n.refresh(context, newLocale);
+                          setState(() {});
+                        },
+                      )
+                    ),
+                  ]
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Notification Settings",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                SwitchListTile(
+                  activeColor: Colors.red,
+                  contentPadding: const EdgeInsets.all(0),
+                  value: _isOn1,
+                  onChanged: (bool value) {
+                    if (value) {
+                      notification.sendNotificationNow(
+                        "Test Notification",
+                        "This is an instant notification",
+                        // "Received"
+                      );
+                    }
+                    setState(() {
+                      _isOn1 = value;
+                    });
+                  },
+                  title: Text('Instant Notifications'),
+                  secondary:
+                      const Icon(Icons.notification_important, color: Colors.red),
+                ),
+                SwitchListTile(
+                  activeColor: Colors.red,
+                  contentPadding: const EdgeInsets.all(0),
+                  value: _isOn2,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _isOn2 = value;
+                    });
+                  },
+                  title: Text('Featured Persons'),
+                  secondary:
+                      const Icon(Icons.notification_important, color: Colors.red),
+                ),
+              ],
+            )
+          )
+        )
+    );
   }
 }
 
